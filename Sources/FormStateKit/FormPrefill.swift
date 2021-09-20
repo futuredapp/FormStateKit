7ee97aa54424d6bf7fd586b8bb1260cbfe136dbe
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-09-20 15:05:16 +0200

    Update availability of form prefill

--- Sources/FormStateKit/FormPrefill.swift
+++ Sources/FormStateKit/FormPrefill.swift
@@ -1,7 +1,7 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 public struct FormPrefill<Form> {
     let form: Form?
 
